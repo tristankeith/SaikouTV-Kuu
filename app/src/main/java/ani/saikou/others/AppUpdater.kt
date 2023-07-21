aec2690b2c3a6013cbddbfcc3586b1ecda905dd3
kemuri <60441676+kemuriXD@users.noreply.github.com>
2023-07-21 11:08:11 +0100

    AppUpdater.kt fix?

--- app/src/main/java/ani/saikou/others/AppUpdater.kt
+++ app/src/main/java/ani/saikou/others/AppUpdater.kt
@@ -22,11 +22,11 @@ object AppUpdater {
         try{
             val version =
                 if(!BuildConfig.DEBUG)
-                    OkHttpClient().newCall(Request.Builder().url("https://raw.githubusercontent.com/Nanoc6/SaikouTV/main/stable.txt").build()).execute().body?.string()?.replace("\n","")?:return
+                    OkHttpClient().newCall(Request.Builder().url("https://raw.githubusercontent.com/kemuriXD/SaikouTV/main/stable.txt").build()).execute().body?.string()?.replace("\n","")?:return
                 else {
                     OkHttpClient().newCall(
                         Request.Builder()
-                            .url("https://raw.githubusercontent.com/Nanoc6/SaikouTV/main/app/build.gradle")
+                            .url("https://raw.githubusercontent.com/kemuriXD/SaikouTV/main/app/build.gradle")
                             .build()
                     ).execute().body?.string()?.substringAfter("versionName \"")?.substringBefore('"') ?: return
                 }
@@ -47,10 +47,10 @@ object AppUpdater {
                                 MainScope().launch(Dispatchers.IO){
 
                                     try{
-                                        OkHttpClient().newCall(Request.Builder().url("https://api.github.com/repos/Nanoc6/SaikouTV/releases/tags/v$version"+"-tv").build()).execute().body?.string()?.apply {
+                                        OkHttpClient().newCall(Request.Builder().url("https://api.github.com/repos/kemuriXD/SaikouTV/releases/tags/v$version"+"-tv").build()).execute().body?.string()?.apply {
                                             substringAfter("\"browser_download_url\":\"").substringBefore('"').apply {
                                                 if (endsWith("apk")) activity.downloadUpdate(this)
-                                                else openLinkInBrowser("https://github.com/Nanoc6/SaikouTV/releases/")
+                                                else openLinkInBrowser("https://github.com/kemuriXD/SaikouTV/releases/")
                                             }
                                         }
                                     }catch (e:Exception){
@@ -58,7 +58,7 @@ object AppUpdater {
                                     }
                                 }
                             }
-                            else openLinkInBrowser( "https://discord.com/channels/902174389351620629/946852010198728704")
+                            else openLinkInBrowser( "https://discord.com/channels/1091762044946092105/1091762908091920455")
                         }
                         setNegativeButton("Cope") { dialogInterface: DialogInterface, _: Int ->
                             dialogInterface.dismiss()
